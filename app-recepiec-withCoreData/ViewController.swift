8d16db9636e3b147e84d07aac9a8be58c13029a1
Khaled Rahman Ayon <khaledrahmanayon@khaleds-air.fritz.box>
2016-07-03 12:22:16 +0200

    Initial Commit

---
+++ app-recepiec-withCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  app-recepiec-withCoreData
+//
+//  Created by Khaled Rahman Ayon on 03/07/16.
+//  Copyright © 2016 iosApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
